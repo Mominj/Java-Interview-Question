66e129edfce689d62629333b8b050bd2467c8b38
momin <mirzamomin8787@gmail.com>
2022-11-03 08:16:11 -0700

    single pattern and syncronization

--- src/com/company/singletonpattern/SingletonMain.java
+++ src/com/company/singletonpattern/SingletonMain.java
@@ -10,12 +10,19 @@ public class SingletonMain {
         System.out.println(singletonLazy1.hashCode());
         System.out.println(singletonLazy.hashCode());
 
+        //syncro
+
+        //eager
+
         SingletonEager singletonEager = SingletonEager.getInstance();
         SingletonEager singletonEager1= SingletonEager.getInstance();
 
-
         System.out.println(singletonEager1.hashCode());
         System.out.println(singletonEager.hashCode());
+
+        //double checked locking
+
+
 
     }
 
