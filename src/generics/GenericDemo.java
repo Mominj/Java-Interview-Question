457e021a619ea3f0457e82f1803ddb895d2817af
Mominj <abdul.momin@sonaliintellect.com>
2023-08-05 13:04:13 +0600

    asdf

--- src/generics/GenericDemo.java
+++ src/generics/GenericDemo.java
@@ -2,8 +2,12 @@ package generics;
 
 public class GenericDemo {
     public static void main(String[] args) {
-        Container<Double> container = new Container<>();
-        container.setValue(10.0);
+//        Container<Double> container = new Container<>();
+//        container.setValue(10.0);
+//        container.typeCheck();
+
+        Container<Integer> container = new Container<>();
+        container.setValue(12);
         container.typeCheck();
 
     }
